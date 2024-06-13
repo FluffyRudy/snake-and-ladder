581e4f06d978ef5f1cf4698df5755a7c9a1353e4
Bishnu Chalise <cryptographycipher29@gmail.com>
2024-06-13 20:26:44 +0545

    ad board size, board position and function to update alpha of color

--- settings.py
+++ settings.py
@@ -9,7 +9,10 @@ NUM_CELLS = 10
 """
 MAKING BOARD SIZE EQUAL TO 70% WIDTH OF SCREEN SIZE
 """
-BOARD_SIZE = int(SCREEN_SIZE * 1.7), int(SCREEN_SIZE * 1.7)
+BOARD_SIZE = int(SCREEN_SIZE * 0.7), int(SCREEN_SIZE * 0.7)
+BOARD_POSITION = int(SCREEN_SIZE // 2 - BOARD_SIZE[0] // 2), int(
+    SCREEN_SIZE // 2 - BOARD_SIZE[0] // 2
+)
 CELL_SIZE = BOARD_SIZE[0] // NUM_CELLS
 
 """
@@ -17,3 +20,7 @@ COLORS
 """
 WHITE = (255, 255, 255, 255)
 BLACK = (0, 0, 0, 255)
+
+
+def update_alpha(color: tuple[int], alpha: int):
+    return (*color[:3], alpha)
