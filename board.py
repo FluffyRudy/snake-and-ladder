1170c35aa36e563acca040dc1e6cab8189af06df
Bishnu Chalise <cryptographycipher29@gmail.com>
2024-06-14 10:37:05 +0545

    create board

--- board.py
+++ board.py
@@ -5,12 +5,70 @@ from settings import (
     CELL_SIZE,
     WHITE,
     BLACK,
-    BOARDER_SIZE,
+    BOARDER_RADIUS,
+    BORDER_WIDTH,
     update_alpha,
+    random_rgb_color,
+    normalized_color,
 )
 import pygame
 
 
 class Board:
     def __init__(self):
-        pass
+        self.font = pygame.font.Font("fonts/Topaz8-xxO8.ttf", 25)
+
+    def draw_grid(self, surface: pygame.Surface):
+        count = 100
+        for y in range(NUM_CELLS):
+            for x in range(NUM_CELLS):
+                posx = BOARD_POSITION[0] + x * CELL_SIZE
+                posy = BOARD_POSITION[1] + y * CELL_SIZE
+                border_radius = self.__get_border_radius(x, y)
+                self.__draw_cell(surface, (posx, posy), **border_radius)
+                _count = (count - x) if y % 2 == 0 else (count - NUM_CELLS + x + 1)
+                text = self.font.render(str(_count), True, WHITE)
+                surface.blit(
+                    text,
+                    (
+                        posx + CELL_SIZE // 2 - text.get_width() // 2,
+                        posy + CELL_SIZE // 2 - text.get_height() // 2,
+                    ),
+                )
+                pygame.draw.circle(
+                    surface,
+                    normalized_color(x, NUM_CELLS),
+                    (posx + CELL_SIZE // 2, posy + CELL_SIZE // 2),
+                    CELL_SIZE // 2 - 2,
+                    BORDER_WIDTH + 2,
+                )
+            count -= NUM_CELLS
+
+    def __draw_cell(
+        self,
+        display_surface: pygame.Surface,
+        pos: tuple[int, int],
+        **border_radius,
+    ):
+        pygame.draw.rect(
+            display_surface,
+            WHITE,
+            (pos, (CELL_SIZE, CELL_SIZE)),
+            BORDER_WIDTH,
+            border_top_left_radius=border_radius.get("tl", 0),
+            border_bottom_left_radius=border_radius.get("bl", 0),
+            border_top_right_radius=border_radius.get("tr", 0),
+            border_bottom_right_radius=border_radius.get("br", 0),
+        )
+
+    def __get_border_radius(self, x, y):
+        radius = {"tl": 0, "bl": 0, "tr": 0, "br": 0}
+        if x == 0 and y == 0:
+            radius["tl"] = BOARDER_RADIUS
+        if x == 0 and y == NUM_CELLS - 1:
+            radius["bl"] = BOARDER_RADIUS
+        if x == NUM_CELLS - 1 and y == 0:
+            radius["tr"] = BOARDER_RADIUS
+        if x == NUM_CELLS - 1 and y == NUM_CELLS - 1:
+            radius["br"] = BOARDER_RADIUS
+        return radius
--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-from settings import SCREEN_SIZE, BLACK
+from settings import SCREEN_SIZE, BLACK, BOARD_POSITION, BOARD_SIZE
+from path_util import join, GRAPHICS_DIRECTORY
 import sys
 import pygame
-from pygame.sprite import Group
-from board import Board
-from pwan import Pwan
+from game_manager import Manager
 
 
 class Game:
@@ -15,6 +14,12 @@ class Game:
         self.screen = pygame.display.set_mode((SCREEN_SIZE, SCREEN_SIZE))
         self.clock = pygame.time.Clock()
 
+        self.bg_image = pygame.transform.scale(
+            pygame.image.load(join(GRAPHICS_DIRECTORY, "bg.jpg")).convert_alpha(),
+            self.screen.get_size(),
+        )
+        self.game_manager = Manager(self.screen)
+
     def handle_event(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -23,8 +28,11 @@ class Game:
 
     def run(self):
         while True:
-            self.screen.fill(BLACK)
+            self.screen.blit(self.bg_image, (0, 0))
             self.handle_event()
+
+            self.game_manager.run()
+
             pygame.display.update()
             self.clock.tick(self.FPS)
 
