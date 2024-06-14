32899dd2a028d2f47ddb93a5263ec28e6b8c729c
Bishnu Chalise <cryptographycipher29@gmail.com>
2024-06-14 10:32:36 +0545

    add pawn counts

--- settings.py
+++ settings.py
@@ -1,9 +1,13 @@
+import os
 import pygame
+from random import randint as rint
+
+PROJECT_DIR = os.path.dirname(__file__)
 
 """
 MAKING SCREEN SQUARE SO THAT ALL CELL WILL BECOME SQUARE
 """
-SCREEN_SIZE = 900
+SCREEN_SIZE = 1000
 
 NUM_CELLS = 10
 """
@@ -14,7 +18,8 @@ BOARD_POSITION = int(SCREEN_SIZE // 2 - BOARD_SIZE[0] // 2), int(
     SCREEN_SIZE // 2 - BOARD_SIZE[0] // 2
 )
 CELL_SIZE = BOARD_SIZE[0] // NUM_CELLS
-BOARDER_SIZE = 5
+BOARDER_RADIUS = 5
+BORDER_WIDTH = 1
 
 """
 COLORS
@@ -23,6 +28,20 @@ WHITE = (255, 255, 255, 255)
 BLACK = (0, 0, 0, 255)
 RED = (255, 0, 0, 255)
 
+"PAWNS"
+MAX_PAWNS = 4
+
 
 def update_alpha(color: tuple[int], alpha: int):
     return (*color[:3], alpha)
+
+
+def random_rgb_color() -> tuple[int, int, int]:
+    return rint(0, 255), rint(0, 255), rint(0, 255)
+
+
+def normalized_color(i: int, N: int):
+    r = int((i / N) * 255)
+    g = int((1 - (i / N)) * 255)
+    b = 128
+    return r, g, b
