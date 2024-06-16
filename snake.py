cf8d0255dc53571ed3371ed8d378c12d3d28a9dc
Bishnu Chalise <cryptographycipher29@gmail.com>
2024-06-16 22:05:34 +0545

    make better snake placement

--- snake.py
+++ snake.py
@@ -1,8 +1,8 @@
 import pygame
-import numpy
+from pygame.math import Vector2
 from pygame.sprite import Sprite, Group
 from random import randrange
-from settings import SNAKES, CELL_SIZE, BOARD_POSITION
+from settings import SNAKES, CELL_SIZE, BOARD_POSITION, BOARD_SIZE
 
 
 class Snake(Sprite):
@@ -14,10 +14,26 @@ class Snake(Sprite):
         self.image = pygame.image.load(self.snakes.pop(random_index))
         if self.image.get_height() <= CELL_SIZE:
             self.image = pygame.transform.scale2x(self.image)
-        self.rect = self.image.get_rect(topleft=pos)
+
+        max_y_limit = BOARD_POSITION[1] + BOARD_SIZE[1] - CELL_SIZE
+        full_rel_height = pos[1] + self.image.get_height()
+        posy_clipped = (
+            pos[1]
+            if full_rel_height <= max_y_limit
+            else max_y_limit - self.image.get_height()
+        )
+
+        max_x_limit = BOARD_POSITION[0] + BOARD_SIZE[0]
+        full_rel_width = pos[0] + self.image.get_width()
+        posx_clipped = (
+            pos[0]
+            if full_rel_width <= max_x_limit
+            else max_x_limit - self.image.get_width()
+        )
+
+        self.rect = self.image.get_rect(topleft=(posx_clipped, posy_clipped))
         self.head_rect = self.get_head_point()
         self.tail_rect = self.get_tail_point()
-        print(self.head_rect, self.tail_rect, self.rect)
 
     def get_head_point(self):
         """
@@ -43,6 +59,7 @@ class Snake(Sprite):
         )
 
     def get_tail_point(self):
+        """only deal with bottom rect"""
         start_x, end_x = 0, self.rect.width
         start_y, end_y = self.rect.height - 1, self.rect.height - 1 - CELL_SIZE
 
@@ -73,6 +90,7 @@ class Snake(Sprite):
         )
 
     def update(self, display_surface: pygame.Surface):
-        pygame.draw.rect(display_surface, "red", self.rect, 3)
-        pygame.draw.rect(display_surface, "blue", self.head_rect, 3)
-        pygame.draw.rect(display_surface, "blue", self.tail_rect, 3)
+        """red area is actual size of image"""
+        # pygame.draw.rect(display_surface, "red", self.rect, 5)
+        pygame.draw.rect(display_surface, "blue", self.head_rect, 5)
+        pygame.draw.rect(display_surface, "green", self.tail_rect, 5)
